7f6b59c49e2ce770879465d695381945e3b8093e
Yash Jain <yashjain@Yashs-MacBook-Air.local>
2021-06-14 00:15:05 -0400

    Created Model Skeletan

--- model.py
+++ model.py
@@ -5,9 +5,17 @@ class PDnet(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-        # Add Conv2d, batch norm, dropout layers
+        self.conv1 = nn.Conv2D(3, 16, 3, padding = 1)
+        self.bn1 = nn.BatchNorm2d(16)
+
+        # Add more Conv2d, pooling, batch norm, dropout layers
+
+        self.pool = nn.MaxPool2d(2,2)
+        self.dropout = nn.Dropout(p = 0.3)
 
     def forward(self, x):
+
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
 
         # do the forward pass through the network
 
