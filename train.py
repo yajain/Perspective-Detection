c9d0c3c4e1fe75d0017a2aa2dcae766092b34803
Yash Jain <yashjain@Yashs-MacBook-Air.local>
2021-06-29 11:04:37 -0400

    provisional training script

---
+++ train.py
@@ -0,0 +1,46 @@
+model = Classifier()
+criterion = nn.NLLLoss(reduction='sum')
+optimizer = optim.Adam(model.parameters(), lr=0.003)
+
+epochs = 30
+
+train_losses, test_losses = [], []
+for e in range(epochs):
+    tot_train_loss = 0
+    for images, labels in trainloader:
+        optimizer.zero_grad()
+        
+        log_ps = model(images)
+        loss = criterion(log_ps, labels)
+        tot_train_loss += loss.item()
+        
+        loss.backward()
+        optimizer.step()
+    else:
+        tot_test_loss = 0
+        test_correct = 0  # Number of correct predictions on the test set
+        
+        # Turn off gradients for validation, saves memory and computations
+        with torch.no_grad():
+            for images, labels in testloader:
+                log_ps = model(images)
+                loss = criterion(log_ps, labels)
+                tot_test_loss += loss.item()
+
+                ps = torch.exp(log_ps)
+                top_p, top_class = ps.topk(1, dim=1)
+                equals = top_class == labels.view(*top_class.shape)
+                test_correct += equals.sum().item()
+
+        # Get mean loss to enable comparison between train and test sets
+        train_loss = tot_train_loss / len(trainloader.dataset)
+        test_loss = tot_test_loss / len(testloader.dataset)
+
+        # At completion of epoch
+        train_losses.append(train_loss)
+        test_losses.append(test_loss)
+
+        print("Epoch: {}/{}.. ".format(e+1, epochs),
+              "Training Loss: {:.3f}.. ".format(train_loss),
+              "Test Loss: {:.3f}.. ".format(test_loss),
+              "Test Accuracy: {:.3f}".format(test_correct / len(testloader.dataset)))
