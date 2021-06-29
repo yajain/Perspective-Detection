e0651fab4a0be0c91e3eb4fecb0bb0ee0929008e
Yash Jain <yashjain@Yashs-MacBook-Air.local>
2021-06-29 11:11:41 -0400

    dataset builder

---
+++ dataset_builder.py
@@ -0,0 +1,35 @@
+import os
+
+years = os.listdir('dataset/')
+
+labels = []
+for year in years:
+    for month in os.listdir('dataset/'+year):
+        for file in os.listdir('dataset/'+year+'/'+month):
+            if ((file.split('.')[-1]) == 'cfx' or (file.split('.')[-1] == 'CFX')):
+                f = open('dataset/'+year+'/'+month+'/'+file, 'r')
+                flag = 0
+                for line in f:
+                    if not(line.find('PERSP(DEF=') == -1):
+                        l = line[line.find('=')+1:-2]
+                        l = l[1:-1]
+                        l = l.replace('(','')
+                        l = l.replace(')','')
+                        a = l.split(',')
+                        a.append('dataset/'+year+'/'+month+'/'+file.split('.')[0]+'_Original')
+                        labels.append(a)
+                        flag = 1
+                    if flag == 1:
+                        break
+
+
+for label in labels:
+    if not(len(label) == 9):
+        label.remove('\t')
+        print(label)
+
+f = open('labels.txt','w')
+for a in labels:
+    f.write(str(a))
+    f.write('\n')
+f.close()
