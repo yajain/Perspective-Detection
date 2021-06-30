6047636fb827df75fc6c3784f3fa8c21d21b81c9
Yash Jain <yashjain@Yashs-MacBook-Air.local>
2021-06-29 20:46:43 -0400

    Adding image path to builder

--- dataset_builder.py
+++ dataset_builder.py
@@ -33,3 +33,15 @@ for a in labels:
     f.write(str(a))
     f.write('\n')
 f.close()
+
+years = os.listdir('dataset/')
+
+img_path = []
+
+for year in years:
+    for month in os.listdir('dataset/'+year):
+        for file in os.listdir('dataset/'+year+'/'+month):
+            if os.path.isdir('dataset/'+year+'/'+month+'/'+file):
+                for image in os.listdir('dataset/'+year+'/'+month+'/'+file):
+                    if ((image.split('.')[0].split('_')[-1] == 'Original') or (image.split('.')[0].split('_')[-1] == 'Original')):
+                        img_path.append('datatset/'+year+'/'+month+'/'+file+'/'+image)
