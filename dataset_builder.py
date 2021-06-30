dbcc0d7d7dd4c9da18a7d6b8a160e549e4327bdb
Yash Jain <yashjain@Yashs-MacBook-Air.local>
2021-06-30 03:37:51 -0400

    Full dataset builder

--- dataset_builder.py
+++ dataset_builder.py
@@ -2,41 +2,7 @@ import os
 
 years = os.listdir('dataset/')
 
-labels = []
-for year in years:
-    for month in os.listdir('dataset/'+year):
-        for file in os.listdir('dataset/'+year+'/'+month):
-            if ((file.split('.')[-1]) == 'cfx' or (file.split('.')[-1] == 'CFX')):
-                f = open('dataset/'+year+'/'+month+'/'+file, 'r')
-                flag = 0
-                for line in f:
-                    if not(line.find('PERSP(DEF=') == -1):
-                        l = line[line.find('=')+1:-2]
-                        l = l[1:-1]
-                        l = l.replace('(','')
-                        l = l.replace(')','')
-                        a = l.split(',')
-                        a.append('dataset/'+year+'/'+month+'/'+file.split('.')[0]+'_Original')
-                        labels.append(a)
-                        flag = 1
-                    if flag == 1:
-                        break
-
-
-for label in labels:
-    if not(len(label) == 9):
-        label.remove('\t')
-        print(label)
-
-f = open('labels.txt','w')
-for a in labels:
-    f.write(str(a))
-    f.write('\n')
-f.close()
-
-years = os.listdir('dataset/')
-
-img_path = []
+img_paths = []
 
 for year in years:
     for month in os.listdir('dataset/'+year):
@@ -44,4 +10,52 @@ for year in years:
             if os.path.isdir('dataset/'+year+'/'+month+'/'+file):
                 for image in os.listdir('dataset/'+year+'/'+month+'/'+file):
                     if ((image.split('.')[0].split('_')[-1] == 'Original') or (image.split('.')[0].split('_')[-1] == 'Original')):
-                        img_path.append('datatset/'+year+'/'+month+'/'+file+'/'+image)
+                        img_paths.append('dataset/'+year+'/'+month+'/'+file+'/'+image)
+
+abels = []
+
+for image in img_paths:
+    label_file = image.split('/')
+    png = label_file[-1]
+    cfx = png.split('.')[0].replace('_Original', '.cfx')
+    label_file = label_file[0:-2]
+    label_file = '/'.join(label_file)
+    label_file = label_file+'/'+cfx
+    if os.path.exists(label_file):
+        labels.append(label_file)
+    else:
+        labels.append('No file')
+
+targets = []
+
+for label in labels:
+    if os.path.exists(label):
+        f = open(label, 'r')
+        flag = 0
+        for line in f:
+            if not(line.find('PERSP(DEF=') == -1):
+                l = line[line.find('=')+1:-2]
+                l = l[1:-1]
+                l = l.replace('(','')
+                l = l.replace(')','')
+                a = l.split(',')
+                targets.append([a, label])
+                flag = 1
+            if flag == 1:
+                break
+    else:
+        targets.append('No file')
+
+count = 0
+for label in targets:
+    if (label == 'No file'):
+        count = count + 1
+
+for i in range(len(labels)):
+    if labels[i] == 'No file':
+        img_paths[i] = 'No file'
+
+for i in range(418):
+    img_paths.remove('No file')
+    labels.remove('No file')
+    targets.remove('No file')
