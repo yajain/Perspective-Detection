73697750309a5e810579066691f4971ab674a018
Yash Jain <yashjain@Yashs-MacBook-Air.local>
2021-07-06 10:36:50 -0400

    Finishing dataloader

--- dataloader.py
+++ dataloader.py
@@ -2,27 +2,28 @@ import os
 import cv2
 import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
+#from torch.utils.data import Dataset, DataLoader
 
 class PerspectiveDataset(Dataset):
-    def __init__(self, imgs_path, label_file):
-        self.img_dim = (512, 512)
+    def __init__(self, img_paths, label_file):
+        self.img_dim = (1024, 1024)
         self.data = []
-
-        # Make a list of all the labels from label_file and store in labels
-
-        for index, image in enumerate(os.listdir(imgs_path)):
-            self.data.append([image, labels[index]])
+        df = pd.read_csv('data.csv', header = None)
+        for index, image in enumerate(os.listdir(img_paths)):
+            self.data.append([image, list(df.iloc(index)))
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        img_path, label = self.data[idx]
+        img_path, target = self.data[idx]
         img = cv2.imread(img_path)
         img = cv2.resize(img, self.img_dim)
-        target = np.asarray(label)
         img_tensor = torch.from_numpy(img)
         img_tensor = img_tensor.permute(2, 0, 1)
-        # In this line convert target to a torch tensor
+        label_tensor = torch.tensor(target)
         return img_tensor, label_tensor
+
+if __name__ == "__main__":
+    dataset = PerspectiveDataset()
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=True)
