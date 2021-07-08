498f97a3d11d376723bca3ec9da6e86bce7907b8
Yash Jain <yajain@syr.edu>
2021-07-08 16:50:29 -0400

    FInal Dataloader

--- dataloader.py
+++ dataloader.py
@@ -3,14 +3,12 @@ import cv2
 import numpy as np
 import torch
 import pandas as pd
-from torch.utils.data import Dataset, DataLoader
 
-class PerspectiveDataset(Dataset):
+class PerspectiveDataset(torch.utils.data.Dataset):
     def __init__(self, img_paths, label_file):
         self.img_dim = (512, 512)
         self.data = []
         df = pd.read_csv(label_file, header = None)
-
         with open (img_paths, 'r') as file:
             for index, image in enumerate(file):
                 image = image[0:-1]
@@ -21,11 +19,30 @@ class PerspectiveDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path, target = self.data[idx]
+        
+        # forming image tensors
         img = cv2.imread(img_path)
+        height, width, channels = img.shape
         img = cv2.resize(img, self.img_dim)
         img_tensor = torch.from_numpy(img)
         img_tensor = img_tensor.permute(2, 0, 1)
+        
+        # normalizing image tensors to be between 0 and 1
+        img_tensor = (img_tensor - img_tensor.min())/img_tensor.max()
+        
+        # forming label tensors
         label_tensor = torch.tensor(target)
+        
+        # adjusting label coords according to default image dimension
+        for i in range(8):
+            if i%2 == 0:
+                label_tensor[i] = label_tensor[i]*self.img_dim[0]/width
+            else:
+                label_tensor[i] = label_tensor[i]*self.img_dim[1]/height
+                
+        # normalizing label tensors to be between 0 and 1
+        label_tensor = (label_tensor - label_tensor.min())/label_tensor.max()
+        
         return img_tensor, label_tensor
 
 if __name__ == "__main__":
